6a7587a0908d4b9448c79797e1aa935f628a6f70
Carlos Davi <72183343+Carlos-d4vi@users.noreply.github.com>
2023-04-18 12:04:48 -0300

    Recrutas
    Exercício 1 - Recrutas

---
+++ Exercício1 Recrutas/index.js
@@ -0,0 +1,15 @@
+let nome = " "
+let sobreNome = " "
+let campoDeEstudo = " "
+let anoDeNascimento = " "
+const data = 2023
+
+nome = prompt("Qual seu primeiro nome?")
+sobreNome = prompt("E o seu Sobrenome?")
+campoDeEstudo = prompt("Em qual campo de estudo você está?")
+anoDeNascimento = prompt("Qual seu ano de nascimento?")
+let anoConvertido = 2023 - anoDeNascimento
+alert(nome)
+alert(sobreNome)
+alert(campoDeEstudo)
+alert(anoConvertido)
