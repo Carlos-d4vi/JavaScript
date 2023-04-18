ce3970d4453ee90f61a1b341a3323325ea2a814a
Carlos Davi <72183343+Carlos-d4vi@users.noreply.github.com>
2023-04-18 12:04:16 -0300

    Calculadora
    Exercício 1 - Calculadora

---
+++ Exercício1 Calculadora/index.js
@@ -0,0 +1,11 @@
+let num1 = prompt("Digite o número1")
+let num2 = prompt("Digite o número2")
+
+
+
+let soma = num1 + num2
+let sub = num1 - num2
+let mult = num1 * num2
+let divi = num1 / num2
+
+alert("Soma:"+ soma + "\n\nSubtração" +sub+ "\nMultiplicação:"+mult+ "\nDivisão:"+divi)
